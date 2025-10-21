4c2c3819a95b2e940183de6fdc1de61715e24616
GandiKB <gandik@gmail.com>
2025-10-21 18:09:57 +0200

    ok

--- run_once_install_apps.sh
+++ run_once_install_apps.sh
@@ -2,7 +2,7 @@ curl -sS https://starship.rs/install.sh | sh
 
 echo "Starship installation complete."
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/mkasberg/ghostty-ubuntu/HEAD/install.sh)"
+curl -fsSL https://raw.githubusercontent.com/mkasberg/ghostty-ubuntu/HEAD/install.sh | sh
 
 echo "Ghostty installation complete"
 
