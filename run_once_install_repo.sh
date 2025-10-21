cd4536ea77970a3a66829095f2b8b8adcf26bb8b
GandiKB <gandik@gmail.com>
2025-10-21 18:36:22 +0200

    ok

--- run_once_install_repo.sh
+++ run_once_install_repo.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 curl -fsS -o- https://deb.packages.mattermost.com/setup-repo.sh | sudo bash
+
+sudo  wget -O /usr/share/keyrings/dbeaver.gpg.key https://dbeaver.io/debs/dbeaver.gpg.key
+echo "deb [signed-by=/usr/share/keyrings/dbeaver.gpg.key] https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
+sudo apt-get update && sudo apt-get install dbeaver-ce
