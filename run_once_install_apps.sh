f6b44179a03f46c7b465d1fdaa44c8972325e6e2
GandiKB <gandik@gmail.com>
2025-10-22 18:56:44 +0200

    add ghostty app

--- run_once_install_apps.sh
+++ run_once_install_apps.sh
@@ -8,3 +8,6 @@ curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
 
 echo "Atuin installation complete"
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/mkasberg/ghostty-ubuntu/HEAD/install.sh)"
+
+echo "Ghostty installation complete"
