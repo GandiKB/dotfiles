c8847a3f58e95748eb71a5e0d9afbec257802a0c
GandiKB <gandik@gmail.com>
2025-10-22 17:13:25 +0200

    add atuin

--- run_once_install_apps.sh
+++ run_once_install_apps.sh
@@ -4,7 +4,7 @@ curl -sS https://starship.rs/install.sh | sh
 
 echo "Starship installation complete."
 
-sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin
+curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
 
 echo "Atuin installation complete"
 
